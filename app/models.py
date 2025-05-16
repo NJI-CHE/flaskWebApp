2cf0d9b69cbdd855a497e1b7d3dc67f05aeaecfd
njiche.dev <njicornelius@gmail.com>
2025-05-16 06:24:04 +0100

    1. Resolved the id issues i was facing from the user table 2. I added manual id to each user created 3. Learnt about transaction,session, migrations 4. Created decorator for flash shell

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ import sqlalchemy.orm as so
 from app import db
 
 class User(db.Model):
-    id: so.Mapped[str] = so.mapped_column(primary_key=True, )
+    id: so.Mapped[str] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
