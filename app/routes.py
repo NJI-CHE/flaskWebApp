9908e5bcb55fc2ca8467f285e72ce53d81bc7469
njiche.dev <njicornelius@gmail.com>
2025-06-03 18:54:33 +0100

    Worked on User sessions, login, logout

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
 
 
 #Decorator modifies the fxn it follows, the @app.route decorator
@@ -22,6 +27,22 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
 #renderinng is the process that converts a template in2 a HTMl page
 #This is done by imporx the inbuilt render_template() fxn
 #this fxn takes a template filename & a varibale list of template
@@ -39,3 +60,8 @@ def login():
         flash('Login requested for user {}, remember_me{}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@pp.route('logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
